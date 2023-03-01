be3e6395ccdc853437bed59ebe10144f4543122d
SimeonBauman <96099765+SimeonBauman@users.noreply.github.com>
2023-03-01 13:22:28 -0600

    stuff

--- CSC 250 Game/Assets/SquareBrain.cs
+++ CSC 250 Game/Assets/SquareBrain.cs
@@ -8,7 +8,7 @@ public class SquareBrain : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //transform.rotation = new Quaternion(transform.rotation.x, transform.rotation.y + 1, transform.rotation.z + 1, transform.rotation.w+1);
     }
 
     private void OnTriggerEnter(Collider other)
